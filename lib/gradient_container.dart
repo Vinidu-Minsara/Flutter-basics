0662ffae11535b3afaf1d7e239eebf30c6cce777
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-02-11 13:34:31 +0530

    init project

---
+++ lib/gradient_container.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+// using one by one colors
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:  BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color1,
+            color2
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello World!'),
+      ),
+    );
+  }
+}
+
+// using list of colors
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration:  BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
